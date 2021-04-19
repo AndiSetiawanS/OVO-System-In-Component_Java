55de6f86053780533f9977fc95506c7020046417
AndiSetiawanS <62185347+AndiSetiawanS@users.noreply.github.com>
2021-04-19 23:54:59 +0700

    Update FirstMedia.java

--- OVO/FirstMedia.java
+++ OVO/FirstMedia.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class FirstMedia implements Bayar {
     public void MemprosesData() {
-        int nomor = 0;
         Scanner NomorPelanggan = new Scanner(System.in);
+        String nomor = "12345";
 
         System.out.print("Masukkan Nomor Pelanggan : ");
-        nomor = NomorPelanggan.nextInt();
+        nomor = NomorPelanggan.nextLine();
 
-        if(nomor != 0){
+        if(nomor.equals("12345")){
             System.out.println("Pembayaran Internet & TV Kabel First Media Berhasil dilakukan");
         } else {
         System.out.println("Nomor Pelanggan Yang Anda Masukkan Salah");
